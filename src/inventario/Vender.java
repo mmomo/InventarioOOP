a3349d899ce1b02ab61f8a565fa4c57f15414d34
César Venzor <cesarvenzor@protonmail.com>
2018-04-11 20:25:33 -0600

    enum de descuentos en interface

---
+++ src/inventario/Vender.java
@@ -0,0 +1,21 @@
+
+package inventario;
+
+public interface Vender {
+    public enum Descuentos {
+        ACCESORIO (0.05),
+        COMPUTADORA (0.1);
+        
+        private final double descuento;
+        
+        Descuentos (double desc) {
+            this.descuento = desc;
+        }
+        
+        public double getDescuento () {
+            return this.descuento;
+        }
+    }
+    
+    abstract void venta ();
+}
