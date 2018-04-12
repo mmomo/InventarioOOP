eeabb2205ec670b868bf012d85f92758c1e5f4ba
César Venzor <cesarvenzor@protonmail.com>
2018-04-11 21:09:31 -0600

    implementacion venta () en clase Accesorio

--- src/inventario/Accesorio.java
+++ src/inventario/Accesorio.java
@@ -1,7 +1,7 @@
 
 package inventario;
 
-public class Accesorio extends Producto {
+public class Accesorio extends Producto implements Vender{
 
     public Accesorio(String nombre, String marca, double costo, int cantidad) {
         super(nombre, marca, costo, cantidad);
@@ -9,8 +9,20 @@ public class Accesorio extends Producto {
     }
 
     @Override
-    void mostrarInfo() {
+    public void mostrarInfo() {
         System.out.println("Accesorios");
     }
     
+    @Override
+    public void venta () {
+        if (this.getCantidad() > 0) {
+            double desc = Math.round(this.getCosto() * Descuentos.ACCESORIO.getDescuento());
+            System.out.println("Costo: " + this.getCosto());
+            System.out.println("Descuento: " + desc);
+            System.out.println("Total: " + (this.getCosto() - desc));
+            this.setCantidad(this.getCantidad() - 1);
+        } else {
+            System.out.println("No hay en existencia");
+        }
+    }
 }
