3552be64d66bc839c1db5298c13d0ba74154d291
César Venzor <cesarvenzor@protonmail.com>
2018-04-11 20:37:49 -0600

    acceso a atributos de clase Computadora arreglado

--- src/inventario/Computadora.java
+++ src/inventario/Computadora.java
@@ -9,12 +9,12 @@ public class Computadora extends Producto implements Vender {
     
     @Override
     public void venta () {
-        if (this.cantidad > 0) {
-            double desc = Math.round(this.costo * Descuentos.COMPUTADORA.getDescuento());
-            System.out.println("Costo: " + this.costo);
+        if (this.getCantidad() > 0) {
+            double desc = Math.round(this.getCosto() * Descuentos.COMPUTADORA.getDescuento());
+            System.out.println("Costo: " + this.getCosto());
             System.out.println("Descuento: " + desc);
-            System.out.println("Total: " + this.costo - desc);
-            this.cantidad--;
+            System.out.println("Total: " + (this.getCosto() - desc));
+            this.setCantidad(this.getCantidad() - 1);
         } else {
             System.out.println("No hay en existencia");
         }
